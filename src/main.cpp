40caeb867a2f414b58aee4ce0463737101b7e0df
Arturacu <arturoacuaviva@gmail.com>
2020-12-05 15:20:39 +0100

    Wrapping up the sensor reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
+/**
+ *
+ * Mercury Smart Shelf
+ * HX711 load cell amplifier with Arduino
+ * https://github.com/Mercury-Smartstores/Mercury-Smart-Shelf
+ *
+ * This file uses the HX711 library from Bogdan Necula.
+ *
+ * MIT License
+ * (c) 2020 Mercury
+ *
+**/
 #include <Arduino.h>
 #include <HX711.h>
 
@@ -5,20 +17,20 @@
 const int LOADCELL_DOUT_PIN = A1;
 const int LOADCELL_SCK_PIN = A0;
 
-// Scale = Weight read / Real weight
-const long LOADCELL_DIVIDER = 5895655;
+const float LOADCELL_DIVIDER = 1; // Real weight = Weight read / LOADCELL_DIVIDER
 
-HX711 loadcell;
+const byte READINGS = 10;
+
+const HX711 loadcell;
+
 
 void setup() {
     Serial.begin(9600);
     loadcell.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-    loadcell.set_scale(LOADCELL_DIVIDER);
+    loadcell.set_scale(LOADCELL_DIVIDER); // Scale = Weight read / Real weight
     loadcell.tare();
 }
 
 void loop() {
-    Serial.print("Peso: ");
-    Serial.println(loadcell.get_units(20));
-    delay(1000);
+    Serial.println(loadcell.get_units(READINGS));
 }
